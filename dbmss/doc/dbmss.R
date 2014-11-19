6f847c7d1fb539a41cc9b6cf07551d46ebe90898
lab1 <bwtian@gmail.com>
2014-11-19 16:01:58 +0900

    / ‘BH/DESCRIPTION’ / ‘BH/MD5’ / ‘BH/Meta/Rd.rds’ / ‘BH/Meta/hsearch.rds’ / ‘BH/Meta/links.rds’ / ‘BH/Meta/nsInfo.rds’ / ‘BH/Meta/package.rds’ / ‘BH/NEWS.Rd’ / ‘BH/help/BH.rdb’ / ‘BH/help/BH.rdx’ ...

--- dbmss/doc/dbmss.R
+++ dbmss/doc/dbmss.R
@@ -1,4 +1,3 @@
-
 ## ----Declarations, echo=FALSE, include=FALSE-----------------------------
 library(knitr)
 # set global chunk options
@@ -6,11 +5,9 @@ opts_chunk$set(cache=TRUE, warning=FALSE, tidy=FALSE, fig.width=8, fig.height=6,
 par(mar=c(0,0,0,0))
 library(dbmss)
 
-
 ## ----SimpleWmppp---------------------------------------------------------
 Pattern <- wmppp(data.frame(X=runif(100), Y=runif(100)))
 summary(Pattern)
-
 
 ## ----CSBIGS, echo=FALSE, results='hide'----------------------------------
 load("CSBIGS.Rdata")
@@ -19,7 +16,6 @@ Category <- cut(Emergencies$M, quantile(Emergencies$M, c(0, 0.9, 1)),
 X <- wmppp(data.frame(X=Emergencies$X, Y=Emergencies$Y, PointType=Category),
    win=Region)
 X$window$units <- c("meter","meters")
-
 
 ## ----Toulouse, echo=FALSE, results='hide', fig.height=4, fig.width=6-----
 load("CSBIGS.Rdata")
@@ -38,18 +34,16 @@ plot(X2$Biggest, main="", maxsize=1)
 text(514300,  1826800, "b")
 par(mfrow=c(1,1))
 
-
 ## ----KdCode, eval=FALSE--------------------------------------------------
-## load("CSBIGS.Rdata")
-## Category <- cut(Emergencies$M, quantile(Emergencies$M, c(0, 0.9, 1)),
-##    labels = c("Other", "Biggest"), include.lowest = TRUE)
-## X <- wmppp(data.frame(X=Emergencies$X, Y=Emergencies$Y, PointType=Category),
-##    win=Region)
-## X$window$units <- c("meter","meters")
-## KdE <- KdEnvelope(X, r=seq(0, 10000, 100), NumberOfSimulations=1000,
-##    ReferenceType="Biggest", Global=TRUE)
-## plot(KdE, main="")
-
+#  load("CSBIGS.Rdata")
+#  Category <- cut(Emergencies$M, quantile(Emergencies$M, c(0, 0.9, 1)),
+#     labels = c("Other", "Biggest"), include.lowest = TRUE)
+#  X <- wmppp(data.frame(X=Emergencies$X, Y=Emergencies$Y, PointType=Category),
+#     win=Region)
+#  X$window$units <- c("meter","meters")
+#  KdE <- KdEnvelope(X, r=seq(0, 10000, 100), NumberOfSimulations=1000,
+#     ReferenceType="Biggest", Global=TRUE)
+#  plot(KdE, main="")
 
 ## ----KdFig, echo=FALSE, results='hide'-----------------------------------
 load("CSBIGS.Rdata")
@@ -62,24 +56,20 @@ KdE <- KdEnvelope(X, r=seq(0, 10000, 100), NumberOfSimulations=1000,
    ReferenceType="Biggest", Global=TRUE)
 plot(KdE, main="")
 
-
 ## ----P16Code, eval=FALSE-------------------------------------------------
-## data("paracou16")
-## plot(paracou16, which.marks="PointWeight", main="")
-
+#  data("paracou16")
+#  plot(paracou16, which.marks="PointWeight", main="")
 
 ## ----P16Fig, echo=FALSE, results='hide'----------------------------------
 par(mar=c(0,1,0,4))
 data("paracou16")
 plot(paracou16, which.marks="PointWeight", main="")
 
-
 ## ----MCode, echo=TRUE, eval=FALSE----------------------------------------
-## Envelope <- MEnvelope(paracou16, r = seq(0, 30, 2), NumberOfSimulations
-##    = 1000, Alpha = 0.05, ReferenceType = "V. Americana", NeighborType
-##    = "Q. Rosea", SimulationType = "RandomLabeling", Global = TRUE)
-## plot(Envelope, main="")
-
+#  Envelope <- MEnvelope(paracou16, r = seq(0, 30, 2), NumberOfSimulations
+#     = 1000, Alpha = 0.05, ReferenceType = "V. Americana", NeighborType
+#     = "Q. Rosea", SimulationType = "RandomLabeling", Global = TRUE)
+#  plot(Envelope, main="")
 
 ## ----MFig, echo=FALSE, results='hide'------------------------------------
 Envelope <- MEnvelope(paracou16, r = seq(0, 30, 2), NumberOfSimulations 
@@ -87,15 +77,12 @@ Envelope <- MEnvelope(paracou16, r = seq(0, 30, 2), NumberOfSimulations
    = "Q. Rosea", SimulationType = "RandomLabeling", Global = TRUE)
 plot(Envelope, main="")
 
-
 ## ----MEnvelope-----------------------------------------------------------
 GoFtest(Envelope)
-
 
 ## ----Ktest---------------------------------------------------------------
 data("paracou16")
 RectWindow <- owin(c(300, 400), c(0, 150))
 X <- paracou16[RectWindow]
 Ktest(X, seq(5, 50, 5))
-
 
--- lpSolveAPI/include/myblas.h
+++ lpSolveAPI/include/myblas.h
@@ -10,34 +10,14 @@
 /* Changes: 19 September 2004   Moved function pointer variable             */
 /*                              declarations from myblas.h to myblas.c      */
 /*                              to avoid linker problems with the Mac.      */
-/*          20 April 2005       Modified all double types to LPSREAL to self-  */
+/*          20 April 2005       Modified all double types to REAL to self-  */
 /*                              adjust to global settings.  Note that BLAS  */
 /*                              as of now does not have double double.      */
+/*          11/11/2014          Modified for R package.                     */
 /* ************************************************************************ */
 
 #define BLAS_BASE         1
-#define UseMacroVector
-#if defined LoadableBlasLib
-#  if LoadableBlasLib == 0
-#    undef LoadableBlasLib
-#  endif
-#else
-#  define LoadableBlasLib
-#endif
-
-
-/* ************************************************************************ */
-/* Include necessary libraries                                              */
-/* ************************************************************************ */
 #include "commonlib.h"
-#ifdef LoadableBlasLib
-  #ifdef WIN32
-    #include <windows.h>
-  #else
-    #include <dlfcn.h>
-  #endif
-#endif
-
 
 #ifdef __cplusplus
 extern "C" {
@@ -48,31 +28,6 @@ extern "C" {
 /* BLAS functions                                                           */
 /* ************************************************************************ */
 
-#ifndef BLAS_CALLMODEL
-#ifdef WIN32
-# define BLAS_CALLMODEL _cdecl
-#else
-# define BLAS_CALLMODEL
-#endif
-#endif
-
-typedef void   (BLAS_CALLMODEL BLAS_dscal_func) (int *n, LPSREAL *da, LPSREAL *dx, int *incx);
-typedef void   (BLAS_CALLMODEL BLAS_dcopy_func) (int *n, LPSREAL *dx, int *incx,  LPSREAL *dy, int *incy);
-typedef void   (BLAS_CALLMODEL BLAS_daxpy_func) (int *n, LPSREAL *da, LPSREAL *dx, int *incx,  LPSREAL *dy, int *incy);
-typedef void   (BLAS_CALLMODEL BLAS_dswap_func) (int *n, LPSREAL *dx, int *incx,  LPSREAL *dy, int *incy);
-typedef double (BLAS_CALLMODEL BLAS_ddot_func)  (int *n, LPSREAL *dx, int *incx,  LPSREAL *dy, int *incy);
-typedef int    (BLAS_CALLMODEL BLAS_idamax_func)(int *n, LPSREAL *x,  int *is);
-typedef void   (BLAS_CALLMODEL BLAS_dload_func) (int *n, LPSREAL *da, LPSREAL *dx, int *incx);
-typedef double (BLAS_CALLMODEL BLAS_dnormi_func)(int *n, LPSREAL *x);
-
-#ifndef __WINAPI
-# ifdef WIN32
-#  define __WINAPI WINAPI
-# else
-#  define __WINAPI
-# endif
-#endif
-
 void init_BLAS(void);
 MYBOOL is_nativeBLAS(void);
 MYBOOL load_BLAS(char *libname);
@@ -81,48 +36,11 @@ MYBOOL unload_BLAS(void);
 /* ************************************************************************ */
 /* User-callable BLAS definitions (C base 1)                                */
 /* ************************************************************************ */
-void dscal ( int n, LPSREAL da,  LPSREAL *dx, int incx );
-void dcopy ( int n, LPSREAL *dx, int incx, LPSREAL *dy, int incy );
-void daxpy ( int n, LPSREAL da,  LPSREAL *dx, int incx,   LPSREAL *dy, int incy );
-void dswap ( int n, LPSREAL *dx, int incx, LPSREAL *dy, int incy );
-LPSREAL ddot  ( int n, LPSREAL *dx, int incx, LPSREAL *dy, int incy );
-int  idamax( int n, LPSREAL *x,  int is );
-void dload ( int n, LPSREAL da,  LPSREAL *dx, int incx );
-LPSREAL dnormi( int n, LPSREAL *x );
 
-
-/* ************************************************************************ */
-/* Locally implemented BLAS functions (C base 0)                            */
-/* ************************************************************************ */
-void BLAS_CALLMODEL my_dscal ( int *n, LPSREAL *da, LPSREAL *dx,  int *incx );
-void BLAS_CALLMODEL my_dcopy ( int *n, LPSREAL *dx, int *incx, LPSREAL *dy, int *incy );
-void BLAS_CALLMODEL my_daxpy ( int *n, LPSREAL *da, LPSREAL *dx,  int *incx,  LPSREAL *dy, int *incy );
-void BLAS_CALLMODEL my_dswap ( int *n, LPSREAL *dx, int *incx, LPSREAL *dy, int *incy );
-LPSREAL BLAS_CALLMODEL my_ddot  ( int *n, LPSREAL *dx, int *incx,  LPSREAL *dy, int *incy );
-int  BLAS_CALLMODEL my_idamax( int *n, LPSREAL *x,  int *is );
-void BLAS_CALLMODEL my_dload ( int *n, LPSREAL *da, LPSREAL *dx, int *incx );
-LPSREAL BLAS_CALLMODEL my_dnormi( int *n, LPSREAL *x );
-
-
-/* ************************************************************************ */
-/* Subvector and submatrix access routines (Fortran compatibility)          */
-/* ************************************************************************ */
-#ifdef UseMacroVector
-  #define subvec(item) (item - 1)
-#else
-  int subvec( int item );
-#endif
-
-int submat( int nrowb, int row, int col );
-int posmat( int nrowb, int row, int col );
-
-
-/* ************************************************************************ */
-/* Randomization functions                                                  */
-/* ************************************************************************ */
-void randomseed(int *seeds);
-void randomdens( int n, LPSREAL *x, LPSREAL r1, LPSREAL r2, LPSREAL densty, int *seeds);
-void ddrand( int n, LPSREAL *x, int incx, int *seeds );
+void lps_dscal ( int n, LPSREAL da,  LPSREAL *dx, int incx );
+void lps_dcopy ( int n, LPSREAL *dx, int incx, LPSREAL *dy, int incy );
+void lps_daxpy ( int n, LPSREAL da,  LPSREAL *dx, int incx,   LPSREAL *dy, int incy );
+int  lps_idamax ( int n, LPSREAL *x,  int is );
 
 
 #ifdef __cplusplus
